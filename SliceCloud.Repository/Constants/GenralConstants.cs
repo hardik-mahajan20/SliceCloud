c7d8419d1e553681230428f1319e78bc61929d51
Hardik Mahajan <hardikmahajan2010@gmail.com>
2026-02-20 16:36:14 +0530

    feat :- added constatns file

---
+++ SliceCloud.Repository/Constants/GenralConstants.cs
@@ -0,0 +1,24 @@
+namespace SliceCloud.Repository.Constants;
+
+/// <summary>
+/// Contains string constants for   general purpose.
+/// Used to avoid magic strings and ensure consistency across the project.
+/// </summary>
+public static class GenralConstants
+{
+    public const string ACCESS_DENIED = "Access Denied.";
+    public const string ERROR = "Error";
+    public const string UNAUTHORIZED = "You are Unauthorized.";
+    public const string X_REQUESTED_WITH = "X-Requested-With";
+    public const string XML_HTTP_REQUEST = "XMLHttpRequest";
+    public const string CONTROLLER = "controller";
+    public const string QR_REDIRECT = "QRRedirect";
+    public const string QR_CODE = "QRCode";
+    public const string QR_MENU = "QRMenu";
+    public const string LOGIN = "Login";
+    public const string AUTH = "Auth";
+    public const string HOME = "Home";
+    public const string AUTH_TOKEN = "AuthToken";
+    public const string USER_DATA = "UserData";
+    public const string TOAST = "Toast";
+}
